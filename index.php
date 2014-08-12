f2087b85e13e0930b68960e69ec87d57fd5ac74c
Retaker <i@retaker.me>
2014-08-12 15:25:53 +0800

    不在页面上显示cnzz图标

--- index.php
+++ index.php
@@ -47,5 +47,6 @@
 		</center>
 		<?php } ?>
 		<script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1253017589'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s95.cnzz.com/z_stat.php%3Fid%3D1253017589%26show%3Dpic' type='text/javascript'%3E%3C/script%3E"));</script>
+		<script type="text/javascript">document.getElementsByTagName("img")[1].style.display="none";</script>
 	</body>
 </html>
